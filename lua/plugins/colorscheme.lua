3db43f70caf032edd81de4b63f51e52c7de814cd
max.dinh <maxdinh14@gmail.com>
2023-05-21 23:39:24 +0700

    init lazyvim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-wave",
+    },
+  },
+}
